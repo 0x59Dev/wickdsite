58565ec0fb9e5aa277685e445359d4cea1d0ed74
0x59 <wjarochowicz91@gmail.com>
2017-02-13 23:52:47 +0100

    Added post view by slug , and it works!

--- wickedsite/blog/urls.py
+++ wickedsite/blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index, name="INDEX"),
+    url(r'post/(?P<slug>[a-zA-Z]+)/$', views.show_post, name="Entry"),
+    url(r'^$', views.index, name="INDEX"),
 ]
--- wickedsite/blog/views.py
+++ wickedsite/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Post
+
 
 def index(request):
     return HttpResponse("S I E M A ")
+
+#show post by requested slug
+def show_post(request, slug):
+    post = Post.objects.filter(slug=slug)
+    title = post[0].title
+    content = post[0].content
+    return HttpResponse("Post :  %s \n    Contewnt : %s" %(title, content) )
 
